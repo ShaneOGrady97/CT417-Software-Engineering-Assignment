0a013b639b94e11bfffc0d0c092129dcd053c4c6
shaneogrady <shane7278@gmail.com>
2019-11-18 22:43:53 +0000

    assignment fix

--- src/main/java/nuigalway/app/Course.java
+++ src/main/java/nuigalway/app/Course.java
@@ -1,34 +1,20 @@
 package nuigalway.app;
 
-<<<<<<< HEAD
 import org.joda.time.*;
 import java.util.ArrayList;
-=======
-import java.time.LocalDate;
-import java.util.*;
-
->>>>>>> 27a2784277e0c55181985c37ca390ef1f6fc2c7e
 import nuigalway.app.Module;
 import nuigalway.app.Student;
 
 public class Course
 {
 	private String courseName;
-<<<<<<< HEAD
 	private ArrayList<Module> modules = new ArrayList<Module>();
 	private ArrayList<Student> students = new ArrayList<Student>();
 	private DateTime startDate;
 	private DateTime endDate;
 	
-	public Course(String courseName, DateTime startDate, DateTime endDate) {
-=======
-	private List<Module> modules = new ArrayList<Module>();
-	private List<Student> students = new ArrayList<Student>();
-	private LocalDate startDate;
-	private LocalDate endDate;
 	
-	public Course(String courseName, LocalDate startDate, LocalDate endDate) {
->>>>>>> 27a2784277e0c55181985c37ca390ef1f6fc2c7e
+	public Course(String courseName, DateTime startDate, DateTime endDate) {
 		this.courseName = courseName;
 		this.startDate = startDate;
 		this.endDate = endDate;
@@ -46,7 +32,7 @@ public class Course
 		modules.add(module);
 	}
 	
-	public List<Module> getModules(){
+	public ArrayList<Module> getModules(){
 		return modules;
 	}
 	
@@ -54,11 +40,10 @@ public class Course
 		students.add(student);
 	}
 	
-	public List<Student> getStudents(){
+	public ArrayList<Student> getStudents(){
 		return students;
 	}
 	
-<<<<<<< HEAD
 	public void setStartDate(DateTime startDate) {
         this.startDate = startDate;
     }
@@ -72,21 +57,6 @@ public class Course
     }
 
     public DateTime getEndDate() {
-=======
-	public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-	
-	public LocalDate getStartDate() {
-        return startDate;
-    }
-	
-	public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public LocalDate getEndDate() {
->>>>>>> 27a2784277e0c55181985c37ca390ef1f6fc2c7e
         return endDate;
     }
 
